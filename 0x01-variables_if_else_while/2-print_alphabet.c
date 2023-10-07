d3d80da997cc2337ac7ff536d163fa7e26d120b8
Otmane Kasmi <davenamir3@gmail.com>
2023-10-06 19:54:58 -0700

    2

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,18 +1,21 @@
-#include <stdlib.h>
 #include <stdio.h>
-/* more headers goes there */
+#include <stdlib.h>
 
 /* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ *              from 'a' to 'z' using a while loop.
+ *
+ */
 int main(void)
 {
-	char character = 'a';
+    char character = 'a';
 
-	while (character <= 'z')
-	{
-		putchar(character);
-		character++;
-	}
-	putchar('\n');
+    while (character <= 'z')
+    {
+        putchar(character);
+        character++;
+    }
+    putchar('\n');
 
-	return (0);
-}
