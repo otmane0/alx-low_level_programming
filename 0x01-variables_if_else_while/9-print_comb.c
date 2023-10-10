f28dadf95e808af70e3a43762411a527c273db89
Otmane Kasmi <davenamir3@gmail.com>
2023-10-09 19:21:39 -0700

    8_9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,23 +1,26 @@
 #include <stdio.h>
+
 /**
  * main - Entry point
+ *
+ * Description: Prints all the numbers of base 16 in lowercase,
+ * followed by a new line.
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-        int num;
+    int num = 0;
 
-        for (num = 0; num < 10; num++)
-        {
-                putchar(num + '0');
+    for (num; num < 10; num++)
+    {
+        int digit = '0' + num;
+        putchar(digit);
+        putchar(',');
+        putchar(' ');
+    }
 
-                if (num < 9)
-                {
-                        putchar(',');
-                        putchar(' ');
-                }
-        }
+    putchar('\n');
 
-        putchar('\n');
-
+    return (0);
+}
