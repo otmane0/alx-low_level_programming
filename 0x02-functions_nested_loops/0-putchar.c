dfc06e5cffd2548c93340feba1b58d047056954a
Otmane Kasmi <davenamir3@gmail.com>
2023-10-11 09:21:15 -0700

    0

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -3,6 +3,9 @@
 /**
  * main - Entry point
  *
+ * Description: This function prints "_putchar" Followed by a new line
+ * int _putchar(char c)
+ *
  * Return: Always 0 (Success)
  */
 
@@ -11,7 +14,7 @@
 		char str[] = "_putchar";
 		int ch;
 
-		for (ch = 0; ch < 8; ch++)
+		for (ch = 0; ch <= 8; ch++)
 			putchar(str[ch]);
 		_putchar('\n');
 
