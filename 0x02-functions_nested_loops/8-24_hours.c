a904bb63d4fb64398b2aa345d78e1d211d513496
Otmane Kasmi <davenamir3@gmail.com>
2023-10-11 15:06:52 -0700

    7

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,24 +1,21 @@
-#include <main.h>
+#include "main.h"
 
 /**
- * jack_baur - prints every minute of the day 
- *
+ * jack_bauer - Prints every minute of the day.
  */
-
 void jack_bauer(void)
 {
-int min , hr;
+int min, hr;
 
 for (hr = 0; hr <= 23; hr++)
 {
 for (min = 0; min <= 59; min++)
 {
-	_putchar((hr / 10) + 48);
-        _putchar((hr % 10) + 48);
-	_putchar(':');
-	_putchar((min / 10) + 48);
-        _putchar((hr % 10) + 48);
-        _putchar(('\n');
+_putchar((hr / 10) + '0');
+_putchar((hr % 10) + '0');
+_putchar(':');
+_putchar((min / 10) + '0');
+_putchar('\n');
 }
 }
 }
