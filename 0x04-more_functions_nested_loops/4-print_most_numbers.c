f516c92898467388374e9d13c6664a3ec99f797a
Otmane Kasmi <davenamir3@gmail.com>
2023-10-12 16:39:03 -0700

    4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -3,19 +3,15 @@
 /**
  * print_most_numbers - Prints all numbers from 0 to 9 except for 2 and 5.
  */
-
 void print_most_numbers(void)
 {
-char c = 48;
-while (c >= 48 && c <= 57)
+char c = '0';
+while (c <= '9')
 {
-if (c != 50  && c != 52)
+if (c != '2' && c != '5')
 {
 putchar (c);
-
 }
 c++;
-
 }
-
 }
