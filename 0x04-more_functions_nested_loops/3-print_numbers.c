9093b31e36856e8bb61ac6a08d844319e5739e48
Otmane Kasmi <davenamir3@gmail.com>
2023-10-12 17:26:22 -0700

    4

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+
+/**
+ * print_most_numbers - Prints all numbers from 0 to 9 .
+ */
+void print_numbers(void)
+{
+int num;
+for (num = 48; num < 58; num++)
+{
+_putchar (num);
+}
+_putchar ('\n');
+}
