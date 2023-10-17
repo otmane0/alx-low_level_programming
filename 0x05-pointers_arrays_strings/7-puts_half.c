a306458578d7b5c33accfae60788fe674d9656d5
OTMANE <davenamir3@gmail.com>
2023-10-17 03:05:19 +0100

    put half

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * puts_half - half
+ * @str: string
+ */
+void puts_half(char *str)
+{
+	int lin , lop;
+
+	for (lop = 0; str[lop] != '\0'; lop++)
+	;
+	for (lin = lop / 2; str[lin] != '\0'; lin++)
+		putchar(str[lin]);
+	putchar('\n');
+}
