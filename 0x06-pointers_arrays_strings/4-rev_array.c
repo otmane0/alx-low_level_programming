c484595e486ff5af31e2d8a1a85263f03940bb2a
OTMANE <davenamir3@gmail.com>
2023-10-21 01:02:44 +0100

    4

--- 0x06-pointers_arrays_strings/4-rev_array.c
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -11,10 +11,10 @@ void reverse_array(int *a, int n)
 {
 		int i, temp;
 
-		for (i = 0; i < n / 2; i++)
-		{
-				temp = a[i];
-				a[i] = a[n - i - 1];
-				a[n - i - 1] = temp;
-		}
+for (i = 0; i < n / 2; i++)
+{
+temp = a[i];
+a[i] = a[n - i - 1];
+a[n - i - 1] = temp;
+}
 }
