c5576a4b283885600c20db6677a46ab1881e53d1
OTMANE <davenamir3@gmail.com>
2023-10-21 01:17:26 +0100

    6 fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,5 +1,25 @@
 #include "main.h"
 
+/**
+ * is_separator - Checks if a character is a word separator.
+ * @c: The character to check.
+ * Return: 1 if the character is a separator, 0 otherwise.
+ */
+int is_separator(char c)
+{
+	char separators[] = " \t\n,;.!?\"(){}";
+	int i;
+
+	for (i = 0; separators[i] != '\0'; i++)
+	{
+		if (c == separators[i])
+		{
+			return (1);
+		}
+	}
+
+	return (0);
+}
 
 /**
  * cap_string - Capitalizes all words of a string.
@@ -9,7 +29,6 @@
 char *cap_string(char *str)
 {
 	int i = 0;
-
 
 	if (str[i] >= 'a' && str[i] <= 'z')
 	{
