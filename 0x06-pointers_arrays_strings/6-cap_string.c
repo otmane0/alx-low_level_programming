2a8b6ee14f0ff60134f711ddcadd4f22f9bc3a14
OTMANE <davenamir3@gmail.com>
2023-10-21 01:11:34 +0100

    6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,38 @@
+int i;
+
+for (i = 0; separators[i] != '\0'; i++)
+{
+	if (c == separators[i])
+	{
+		return (1);
+	}
+}
+
+return (0);
+}
+
+/**
+ * cap_string - Capitalizes all words of a string.
+ * @str: The input string.
+ * Return: A pointer to the resulting string.
+ */
+char *cap_string(char *str)
+{
+	int i = 0;
+
+	if (str[i] >= 'a' && str[i] <= 'z')
+	{
+		str[i] = str[i] - 'a' + 'A';
+	}
+
+	while (str[i] != '\0')
+	{
+		if (is_separator(str[i]) && (str[i + 1] >= 'a' && str[i + 1] <= 'z'))
+		{
+			str[i + 1] = str[i + 1] - 'a' + 'A';
+		}
+		i++;
+	}
+
+	return (str);
+}
