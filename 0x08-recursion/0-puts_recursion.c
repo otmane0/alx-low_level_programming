3b77dd983974d0a393d046458bce0f5ed7aafab8
OTMANE <davenamir3@gmail.com>
2023-10-26 01:07:30 +0100

    0

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * _puts_recursion - Prints a string followed by a new line using recursion.
+ * @s: the input string.
+ * Return.
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s != '\0')
+	{
+		putchar (*s);
+		_puts_recursion (s + 1);
+	}
+
+	else
+	{
+		putchar('\n');
+	}
+}
