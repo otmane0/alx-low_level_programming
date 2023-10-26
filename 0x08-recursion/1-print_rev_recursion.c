0b6a2b45c9be19616735c341e603407835e875c6
OTMANE <davenamir3@gmail.com>
2023-10-26 01:31:48 +0100

    fix

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * _puts_recursion - Prints a string followed by a new line using recursion.
+ * _print_rev_recursion - Prints a string followed by a new line using recursion.
  * @s: the input string.
  * Return.
  */
