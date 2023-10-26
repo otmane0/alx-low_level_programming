f1efe9c1763bce7e4eaee199f6d52989f2347582
OTMANE <davenamir3@gmail.com>
2023-10-26 01:20:10 +0100

    1

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _print_rev_recursion - Prints a string followed by a new line using recursion.
+ * @s: the input string.
+ * Return.
+ */
+
+void _print_rev_recursion(char *s)
+{
+	if (*s > '\0')
+	{
+		_puts_recursion(s + 1);
+		putchar(*s);
+	}
+
+}
