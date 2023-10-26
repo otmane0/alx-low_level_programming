06d8abaa7c607d4b88d61ea6f2006e80e0555038
OTMANE <davenamir3@gmail.com>
2023-10-26 01:12:18 +0100

    0

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -6,7 +6,7 @@
  * Return.
  */
 
-void _puts_recursion(char *s)
+void _puts_recursion (char *s)
 {
 	if (*s != '\0')
 	{
@@ -16,6 +16,6 @@ void _puts_recursion(char *s)
 
 	else
 	{
-		putchar('\n');
+		putchar ('\n');
 	}
 }
