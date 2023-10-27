0aa5b8d6afddd2e4794b9963f26d60409a996f1a
OTMANE <davenamir3@gmail.com>
2023-10-28 00:08:52 +0100

    0

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -4,10 +4,11 @@
  * main - the main function.
  * @argc: conter of inputs.
  * @argv: the input.
- * Return 0.
+ * Return: 0.
 */
 int main (int argc, char *argv[])
 {
+	(void)argc;
 	printf ("%s\n", argv[0]);
 	return (0);
 }
