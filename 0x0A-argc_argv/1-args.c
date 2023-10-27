7a674d5e27aa3e6ca324ee31ed6529dea9d13732
OTMANE <davenamir3@gmail.com>
2023-10-28 00:58:32 +0100

    1

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -6,11 +6,11 @@
  * @argv: the input.
  * Return: 0.
 */
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
 	(void)argv;
 
-	printf ("%d\n", argc - 1);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
