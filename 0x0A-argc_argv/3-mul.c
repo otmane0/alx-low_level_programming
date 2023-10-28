2f80f6b3095eaeb84799ee59413bbbbe9e584bd6
OTMANE <davenamir3@gmail.com>
2023-10-28 01:58:10 +0100

    3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - the main function.
+ * @argc: conter of inputs.
+ * @argv: the input.
+ * Return: 0.
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 2)
+	{
+		printf("Error");
+		return (1);
+	}
+
+	int num1 = atoi(argv[1]);
+	int num2 = atoi(argv[2]);
+	int sum = num1 * num2;
+
+	printf("%d\n", sum);
+
+	return (0);
+}
