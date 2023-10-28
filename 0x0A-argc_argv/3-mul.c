f6ab7a8a781a774041d974969900f5f1fcfc83a1
OTMANE <davenamir3@gmail.com>
2023-10-28 01:59:52 +0100

    3

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -14,9 +14,12 @@ int main(int argc, char *argv[])
 		return (1);
 	}
 
-	int num1 = atoi(argv[1]);
-	int num2 = atoi(argv[2]);
-	int sum = num1 * num2;
+	else if (argc != 2)
+	{
+		int num1 = atoi(argv[1]);
+		int num2 = atoi(argv[2]);
+		int sum = num1 * num2;
+	}
 
 	printf("%d\n", sum);
 
