1ae8948d4ab2ef210d363dd572027eb196371199
OTMANE <davenamir3@gmail.com>
2023-11-04 03:50:25 +0100

    1

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -31,7 +31,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (n >= len2)
 		n = len2;
-	concatenated = malloc((len1 + n + 1) * sizeof(char);
+	concatenated = malloc((len1 + n + 1) * sizeof(char));
 	if (concatenated == NULL)
 		return (NULL);
 
