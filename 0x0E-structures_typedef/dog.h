73808b68a0ab93a0a7481195143a0a2aea232cb3
OTMANE <davenamir3@gmail.com>
2023-11-07 00:39:26 +0100

    3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -10,12 +10,12 @@
  * @owner: the owner.
 */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-};
+} dog_t;
 
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
