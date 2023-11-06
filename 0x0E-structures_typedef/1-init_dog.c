1734b66e382099307876b50568e8a5c0060f4828
OTMANE <davenamir3@gmail.com>
2023-11-06 18:54:09 +0100

    1

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <dog.h>
+
+/**
+ * init_dog - function for type
+ * @d: type of code.
+ * @name: the name of the dog
+ * @age: the age.
+ * @owner: the owner.
+*/
+void init_dog(struct dog *d, char *name, float age, char *owner)
+{
+	struct dog name;
+	struct dog age;
+	struct dog owner;
+
+}
