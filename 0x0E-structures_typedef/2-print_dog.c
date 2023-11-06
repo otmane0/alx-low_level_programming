48b327a770c4aab54c933a07eeab6a94c04acf40
OTMANE <davenamir3@gmail.com>
2023-11-07 00:08:01 +0100

    2

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+
+#include "dog.h"
+
+/**
+ * init_dog - function for type
+ * @d: type of code.
+ * @name: the name of the dog
+ * @age: the age.
+ * @owner: the owner.
+*/
+
+
+void print_dog(struct dog *d)
+{
+
+printf("Name: %s\nAge: %f\nOwner: %s\n", d->name, d->age, d->owner);
+
+}
--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,8 @@
 #ifndef DOG_H
 #define DOG_H
 
+#include <stdio.h>
+
 /**
  * struct dog - function for type
  * @name: the name of the dog
@@ -15,6 +17,8 @@ struct dog
 	char *owner;
 };
 
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
 
 #endif
