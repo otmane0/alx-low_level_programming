f8aaed924eabf8f6c2773b86601eb0dcf3eef9f0
OTMANE <davenamir3@gmail.com>
2023-11-06 16:49:46 +0100

    0 fix

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,18 @@
-#include <stdio.h>
+#ifndef DOG_H
 
-#ifdef DOG_H
 #define DOG_H
+
+/**
+ * struct dog - for the dog.
+ * @name: the name of the dog
+ * @age: the age.
+ * @owner: the owner.
+*/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
 };
+
 #endif
