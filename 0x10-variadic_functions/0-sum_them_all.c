3949d79e83a5c72e8bbf923f77bd95e749508387
OTMANE <davenamir3@gmail.com>
2023-11-09 14:54:59 +0100

    0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,6 +3,7 @@
 /**
  * sum_them_all - a function.
  * @n: is a int.
+ * Return: 0 if n == 0.
 */
 
 int sum_them_all(const unsigned int n, ...)
@@ -17,8 +18,10 @@ int sum_them_all(const unsigned int n, ...)
 
 	{
 		int sum = 0, i = 0;
+
 		va_list ptr;
-		va_start (ptr, n);
+
+		va_start(ptr, n);
 
 	for (i = 0; i < n; i++)
 	{
