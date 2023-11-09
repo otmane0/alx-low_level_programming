46c518a3583645911abf23bbefe90d21b1608831
OTMANE <davenamir3@gmail.com>
2023-11-09 14:47:59 +0100

    0

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -7,21 +7,23 @@
 
 int sum_them_all(const unsigned int n, ...)
 {
-	int sum = 0, i;
 
 	if (n == 0)
 	{
 		return (0);
 	}
 	else
+
+
 	{
+		int sum = 0, i = 0;
 		va_list ptr;
 		va_start (ptr, n);
 
 	for (i = 0; i < n; i++)
 	{
-		va_arg(ptr, int);
-		sum = sum + ptr;
+
+		sum = sum + va_arg(ptr, int);
 	}
 	return (sum);
 	}
--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -6,4 +6,5 @@
 
 int sum_them_all(const unsigned int n, ...);
 
+
 #endif
