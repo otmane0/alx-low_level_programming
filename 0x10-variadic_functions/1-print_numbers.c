3d602369a8dcd7b817767efeee74d73c9aa6945f
OTMANE <davenamir3@gmail.com>
2023-11-09 23:59:23 +0100

    1

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -20,7 +20,7 @@ unsigned int i;
 
 if (separator == NULL)
 {
-
+	separator = "";
 }
 else
 {
--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -17,7 +17,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	}
 if (separator == NULL)
 {
-
+	separator = "";
 }
 else
 {
