7087a2fe157b4487818429a1238aea87bbb42b4e
OTMANE <davenamir3@gmail.com>
2023-11-09 22:13:34 +0100

    1

---
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -0,0 +1,37 @@
+#include "variadic_functions.h"
+
+/**
+ * print_numbers - function.
+ * @separator: array.
+ * @n: number.
+*/
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+
+
+unsigned int i, j;
+if (separator == NULL)
+{
+	printf("nil");
+}
+else
+{
+va_list ptr;
+va_start(ptr, n);
+
+for (i = 0; i < n ; i++)
+{
+	printf("%d", va_arg(ptr, int));
+
+	if (i < n - 1)
+	{
+	printf("%s", separator);
+
+	}
+
+}
+printf("\n");
+va_end(ptr);
+}
+}
