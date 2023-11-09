672640754db35a942752946ebeaece8ed8822fa3
OTMANE <davenamir3@gmail.com>
2023-11-09 23:24:25 +0100

    2

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,45 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - function.
+ * @separator: array.
+ * @n: number.
+*/
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+if (separator == NULL)
+{
+
+}
+else
+{
+va_list ptr;
+va_start(ptr, n);
+
+
+for (i = 0; i < n ; i++)
+{
+	char *str = va_arg(ptr, char *);
+
+	if (str == NULL)
+	{
+		printf("nill");
+	}
+
+	else
+	{
+		printf("%s", str);
+
+	if (i < n - 1)
+	{
+		printf("%s", separator);
+	}
+	}
+}
+printf("\n");
+va_end(ptr);
+}
+
+}
