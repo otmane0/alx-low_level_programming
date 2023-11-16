03863155eeae6718d4ac34ea2ac21815cbc64dc0
OTMANE <davenamir3@gmail.com>
2023-11-17 00:03:54 +0100

    0

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,56 @@
+
+#include <stdio.h>
+#include <lists.h>
+
+/**
+ * _strlen - a function.
+ * @stringg: string.
+ * @i: int.
+ * Return: E.
+*/
+int _strlen(char *stringg)
+{
+	int i = 0;
+
+	if (stringg)
+	{
+		while (stringg[i] != '\0')
+		{
+			i++;
+		}
+		return (i);
+	}
+	else
+	{
+		return (0);
+	}
+}
+
+/**
+ * print_list - a function.
+ * @str: list.
+ * @h: int.
+ * @list_t: a struct.
+ * Return: E.
+*/
+
+size_t print_list(const list_t *h)
+{
+	size_t i = 0;
+
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+		{
+			printf("(nil)");
+		}
+		else
+		{
+			printf("[%d] %s\n", _strlen(h->str), h->str);
+		}
+		h = h->next;
+		i++;
+
+
+	}
+}
