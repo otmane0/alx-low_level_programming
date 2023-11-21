3f78a302a8869917892c361108ba0df7c640794a
OTMANE <davenamir3@gmail.com>
2023-11-21 23:19:35 +0100

    0

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_listint - func.
+ * @h: array.
+ * Return: 0
+*/
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
