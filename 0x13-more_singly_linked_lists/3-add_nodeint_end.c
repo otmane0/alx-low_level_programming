3e4292645255ab0e578a850e0cc5b1678d639b7a
OTMANE <davenamir3@gmail.com>
2023-11-22 02:27:35 +0100

    3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -1,5 +1,12 @@
 #include "lists.h"
 
+/**
+ * add_nodeint_end - func.
+ * @head: head.
+ * @n: num.
+ * Return: 0
+*/
+
 listint_t *add_nodeint_end(listint_t **head, const int n)
 {
 		listint_t *last_node = malloc(sizeof(listint_t));
