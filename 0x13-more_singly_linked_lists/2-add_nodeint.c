1cacf981d1b8653fd1b0ca58ac01b5e05f79780c
OTMANE <davenamir3@gmail.com>
2023-11-22 01:02:07 +0100

    2

---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+
+/**
+ * add_nodeint - func.
+ * @head: head.
+ * @n: num.
+ * Return: 0
+*/
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *first_node = (listint_t *)malloc(sizeof(listint_t));
+
+	if (head == NULL || first_node == NULL)
+	{
+		return (NULL);
+	}
+
+		first_node->next = NULL;
+		first_node->n = n;
+		if (head != NULL)
+		{
+			first_node->next = *head;
+			*head = first_node;
+			return (first_node);
+		}
+
+}
