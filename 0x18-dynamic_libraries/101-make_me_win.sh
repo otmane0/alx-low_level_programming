8b2bd2a8704419608586e63eb0ed9a6a966b8054
OTMANE <davenamir3@gmail.com>
2024-01-15 23:52:44 +0100

    1

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 wget -P https://github.com/otmane0/alx-low_level_programming/raw/master/0x18-dynamic_libraries/full_lib.so
-export LD_PRELOAD=/tmp/full_lib.so
+export LD_PRELOAD=/home/othmane/alx/alx-low_level_programming/0x18-dynamic_libraries : $LD_PRELOAD
