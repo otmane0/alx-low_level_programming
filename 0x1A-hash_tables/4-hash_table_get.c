260fffd2d28312ede3dce87bb65f0de19ddef170
OTMANE <davenamir3@gmail.com>
2024-02-07 23:45:17 +0100

    test

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -13,23 +13,18 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *temp;
 
-	if (ht == NULL || key == NULL || strlen(key) == 0 ||
-	 ht->array == NULL || ht->size == 0)
-	{
-		return (NULL);
-	}
+	if (ht == NULL || key == NULL || ht->array == NULL || ht->size == 0)
+		return NULL;
 
-	index = key_index((const unsigned char *) key, ht->size);
+	index = key_index((const unsigned char *)key, ht->size);
 	temp = ht->array[index];
+
 	while (temp != NULL)
 	{
 		if (strcmp(temp->key, key) == 0)
-		{
-			return (temp->value);
-		}
+			return temp->value;
 		temp = temp->next;
 	}
 
 	return (NULL);
-
 }
