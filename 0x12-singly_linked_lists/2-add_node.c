57831af1c0371b1047a92be2aeaa95a089b1ddaa
OTMANE <davenamir3@gmail.com>
2024-02-09 12:01:24 +0100

    test

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -11,9 +11,10 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_node = malloc(sizeof(list_t));
 
-	if (new_node == NULL)
+	if (!new_node || !head)
 		return (NULL);
 
+	if (str)
 	{
 		new_node->str = strdup(str);
 		if (new_node->str == NULL)
