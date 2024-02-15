d8a0f22ebd36cf6d08f13e95d3452c7711837bdd
OTMANE <davenamir3@gmail.com>
2024-02-15 21:51:02 +0100

    test

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * add_node - singly linked list
+ * @head: points to the next node
+ * @str: string
+ * Return: num
+ */
+
+list_t *add_node(list_t **head, const char *str)
+{
+
+	list_t *new_node = malloc(sizeof(list_t));
+
+	if (new_node == NULL)
+	{
+		return (NULL);
+	}
+	new_node->str = strdup(str);
+	if (new_node->str == NULL)
+	{
+		free(new_node);
+		return (NULL);
+	}
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+
+}
