ab75d9e578f940c9c69e0219afef5c458693112a
OTMANE <davenamir3@gmail.com>
2024-02-16 20:48:49 +0100

    test

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -1,24 +1,24 @@
 #include "lists.h"
 
 /**
- * *get_nodeint_at_index - singly linked list
+ * sum_listint - singly linked list
  * @head: integer
- * @index: num
  * Return: val
  */
 
 int sum_listint(listint_t *head)
 {
-    int sum = 0;
+	int sum = 0;
 	int data;
-    if (head == NULL)
-        return (0);
-    while(head != NULL)
-    {
-		data = head->n;
-        sum = sum + data;
-		head = head->next;
-    }
 
-    return (sum);
+	if (head == NULL)
+		return (0);
+
+	while (head != NULL)
+	{
+		data = head->n;
+	    sum = sum + data;
+		head = head->next;
+	}
+	return (sum);
 }
