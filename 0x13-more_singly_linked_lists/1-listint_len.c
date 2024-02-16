6cc737a8038c8f6fc2499f039f015361c0a02d61
OTMANE <davenamir3@gmail.com>
2024-02-16 13:35:41 +0100

    test

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,20 +1,22 @@
 #include "lists.h"
 
 /**
- * listint_len - func.
- * @h: array.
- * Return: 01
-*/
+ * listint_len - singly linked list
+ * @h: integer
+ * Return: singly linked list node structure
+ */
 
 size_t listint_len(const listint_t *h)
 {
-	int i = 0;
+	size_t i = 0;
 
+	if (h == NULL)
+		return (0);
 	while (h != NULL)
 	{
-
 		h = h->next;
 		i++;
 	}
+
 	return (i);
 }
