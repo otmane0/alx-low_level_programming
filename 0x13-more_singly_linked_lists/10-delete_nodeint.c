d592de57e889568e2970dc54a03b6c40b978181a
OTMANE <davenamir3@gmail.com>
2024-02-18 12:04:27 +0100

    test

---
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -0,0 +1,41 @@
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - singly linked list
+ * @head: integer
+ * @index: num
+ * Return: val
+ */
+
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	unsigned int counter;
+	listint_t *current = *head, *temp;
+
+	if (*head == NULL)
+		return (-1);
+
+	if (index == 0)
+	{
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+		return (1);
+	}
+
+	while (current != NULL && counter < index - 1)
+	{
+		current = current->next;
+	}
+
+	if (current == NULL || counter != index)
+	{
+		return (-1);
+	}
+
+	temp = current->next;
+	current->next = temp->next;
+	free(temp);
+	return (1);
+
+}
