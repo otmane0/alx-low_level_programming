68bbfcb6b490ee52c840f88d9e12fa9f9e485b12
OTMANE <davenamir3@gmail.com>
2024-02-19 19:14:14 +0100

    test

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -10,13 +10,13 @@
 
 size_t print_dlistint(const dlistint_t *h)
 {
-	int count = 0;
+	size_t count = 0;
 
 	if (h == NULL)
-		return (NULL);
+		return (0);
 	while (h != NULL)
 	{
-		print("%d", h->n);
+		printf("%d\n", h->n);
 		h = h->next;
 		count++;
 	}
