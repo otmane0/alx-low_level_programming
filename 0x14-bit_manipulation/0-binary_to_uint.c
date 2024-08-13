c13d2ce2d9ae7c1ce32ec9483d0b4d5b6f7a5c8e
OTMANE <davenamir3@gmail.com>
2024-08-13 21:19:57 +0100

    test

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,14 +1,14 @@
-
 #include "main.h"
 #include <stdlib.h>
 
 /**
- * binary_to_uint - Reset the value of an integer pointer to 98.
- * @b: A pointer to an s.
- * Return: jds
+ * binary_to_uint - Converts a binary number in string f
+ * @b: A pointer to a string of '0' and '1' characters.
+ *
+ * Return: The converted number, or 0 if:
+ *		 - b is NULL
+ *		 - There are characters in the string that are not '0' or '1'
  */
-
-
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int i = 0, n = 0, j = 0;
@@ -16,25 +16,22 @@ unsigned int binary_to_uint(const char *b)
 	if (b == NULL)
 		return (0);
 
-	for (i; b[i] != '\0'; i++)
-	;
+	while (b[i] != '\0')
+		i++;
 
-	for (; i > 0; i--)
+	while (i > 0)
 	{
+		i--;
 
-		if (b[i - 1] != '0' && b[i - 1] != '1')
+		if (b[i] != '0' && b[i] != '1')
 			return (0);
 
-		if (b[i - 1] == '1')
+		if (b[i] == '1')
 		{
 			j += 1 << n;
 		}
 		n++;
-
 	}
 
 	return (j);
-
-
-
 }
