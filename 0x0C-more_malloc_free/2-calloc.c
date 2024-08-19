fb62fd67ab8b72f8aa622cfdd271db8d407f62ba
OTMANE <davenamir3@gmail.com>
2024-08-19 23:17:56 +0100

    test

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -11,17 +11,17 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	unsigned int i, j, t;
+	unsigned int i, t;
 	unsigned char *array;
 
 	if (nmemb == 0 || size == 0)
 		return (NULL);
 
-	array = malloc(nmemb * (size));
+	t = nmemb * size;
+	array = malloc(t);
 	if (!array)
 		return (NULL);
 
-	t = nmemb * size;
 	for (i = 0; i < t; i++)
 	{
 		array[i] = 0;
@@ -29,19 +29,4 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 
 	return (array);
 
-
-
-
-
-
 }
-
-/**
- * Write a function that allocates memory for an array, using malloc.
-
-Prototype: void *_calloc(unsigned int nmemb, unsigned int size);
-The _calloc function allocates memory for an array of nmemb elements of size bytes each and returns a pointer to the allocated memory.
-The memory is set to zero
-If nmemb or size is 0, then _calloc returns NULL
-If malloc fails, then _calloc returns NULL
-*/
