332422c9cb1edef5c606baf236c01bc6ab66f296
OTMANE <davenamir3@gmail.com>
2024-08-20 19:46:48 +0100

    test

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -4,48 +4,61 @@
  * read_textfile - Reads a text file and print
  * @filename: The name of the file to read.
  * @letters: The number of letters to read and print.
- *
+ * fd: The file pointer to read from(srequest)
  * Return: The actual number of letters
  *         or 0 if the file cannot be opened/read
  */
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int fd;
-	ssize_t bytes_read, bytes_written;
-	char *buffer;
+    FILE *fd;
+    char *array;
+    size_t r_byt, w_byt;
 
-	if (filename == NULL)
-		return (0);
 
-	fd = open(filename, O_RDONLY);
-	if (fd == -1)
-		return (0);
+    if (!filename)
+        return (0);
 
-	buffer = malloc(letters);
-	if (buffer == NULL)
-	{
-		close(fd);
-		return (0);
-	}
+    fd = fopen(filename, "r");
+    if (!fd)
+        return (0);
 
-	bytes_read = read(fd, buffer, letters);
-	if (bytes_read == -1)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
+    array = malloc(letters * sizeof(char));
+    if (!array)
+    {
+        fclose(fd);
+        return (0);
+    }
+    r_byt = fread(array, sizeof(char), letters, fd);
+    if (!r_byt || r_byt == 0)
+    {
+        free(array);
+        fclose(fd);
+        return(0);
+    }
+    if (letters > r_byt)
+    {
+        letters = r_byt;
+    }
 
-	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
-	if (bytes_written == -1 || bytes_written != bytes_read)
-	{
-		free(buffer);
-		close(fd);
-		return (0);
-	}
+    w_byt = fwrite(array, sizeof(char), r_byt, stdout);
+    free(array);
+    fclose(fd);
 
-	free(buffer);
-	close(fd);
+    if (w_byt != r_byt)
+        return (0);
 
-	return (bytes_written);
+    return(r_byt);
+
+
 }
+
+/**
+ * Write a function that reads a text file and prints it to the POSIX standard output.
+
+Prototype: ssize_t read_textfile(const char *filename, size_t letters);
+where letters is the number of letters it should read and print
+returns the actual number of letters it could read and print
+if the file can not be opened or read, return 0
+if filename is NULL return 0
+if write fails or does not write the expected amount of bytes, return 0
+*/
