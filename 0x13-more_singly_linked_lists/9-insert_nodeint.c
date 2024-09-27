da4ce8978a3467651c1410fc93e058846e5d7e26
OTMANE <davenamir3@gmail.com>
2024-09-27 17:04:22 +0100

    test

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -8,39 +8,46 @@
  * Return: val
  */
 
+
+
+
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	unsigned int counter = 0;
-	listint_t *new_node = malloc(sizeof(listint_t));
-	listint_t *change = *head;
+	unsigned int i;
+	listint_t *new_node, *prev, *temp;
 
-	if (new_node == NULL)
-	{
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
 		return (NULL);
-	}
-	if (idx == 0 || (*head) == NULL)
+
+	new_node->n = n;
+	new_node->next = NULL;
+
+		if (!head || idx == 0)
+		{
+			new_node->next = *head;
+			*head = new_node;
+			return (new_node);
+		}
+
+	prev = *head;
+	for (i = 0; prev; i++)
 	{
+		temp = prev;
 
-		new_node->n = n;
-		new_node->next = (*head);
-		(*head) = new_node;
-		return (new_node);
-	}
-	while ((change != NULL) && counter < (idx - 1))
-	{
-		change = change->next;
-		counter++;
-	}
-	if (change == NULL || counter != idx - 1)
-	{
-		free(new_node);
-		return (NULL);
+		if (i == idx - 1)
+		{
+
+			prev = prev->next;
+			temp->next = new_node;
+			new_node->next = prev;
+			return (new_node);
+		}
+
+		prev = prev->next;
 	}
 
-
-	new_node->next = change->next;
-	change->next = new_node;
-
-	return (new_node);
+	free(new_node);
+	return (NULL);
 
 }
